6eb0ac451b54cdb32076ab5b1c2cac33e8e3dd9d
zeta709 <zeta709@gmail.com>
2013-08-07 19:07:34 +0900

    Change tabs to spaces (PEP8): views.py

--- coffee_capsules/views.py
+++ coffee_capsules/views.py
@@ -12,6 +12,7 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
+from django.forms import forms
 from django.forms.models import modelformset_factory
 from django.forms.models import inlineformset_factory
 
@@ -20,214 +21,224 @@ from coffee_capsules.forms import PurchaseForm, MyRequestForm
 
 from django.utils import timezone
 
+
 class IndexView(generic.ListView):
-	template_name = 'coffee_capsules/index.html'
-	context_object_name = 'purchase_list'
-	def get_queryset(self):
-		return Purchase.objects.order_by('-pk')
+    template_name = 'coffee_capsules/index.html'
+    context_object_name = 'purchase_list'
+
+    def get_queryset(self):
+        return Purchase.objects.order_by('-pk')
+
 
 #class DetailView(generic.ListView):
-#	template_name = 'context_object_name/detail.html'
-#	context_object_name = 'purchase_item_list'
-#	def get_queryset(self):
-#		return PurchaseItem.objects.filter(pk=pk)
+#    template_name = 'context_object_name/detail.html'
+#    context_object_name = 'purchase_item_list'
+#    def get_queryset(self):
+#        return PurchaseItem.objects.filter(pk=pk)
 
-from django.forms import forms
+
 @login_required
 @transaction.commit_on_success
 def new_purchase(request):
-	template_name = 'coffee_capsules/new_purchase.html'
-	all_capsule_list = Capsule.objects.order_by('pk')
-	n_of_capsules = len(all_capsule_list)
-	## Use formset factory
-	PurchaseFormSet = modelformset_factory(Purchase, form=PurchaseForm, extra=1)
-	PurchaseItemFormset = inlineformset_factory(Purchase, PurchaseItem,
-						    fk_name="purchase", extra=n_of_capsules)
-	#### POST method
-	if request.method == 'POST':
-		formset = PurchaseFormSet(request.POST, prefix='purchase')
-		formset2 = PurchaseItemFormset(request.POST, prefix='purchaseitem')
-		## For a new instance creation, empty form should not be permitted
-		for i in range(0, formset.total_form_count()):
-		    formset.forms[i].empty_permitted = False
-		## Validation and save
-		has_error = False
-		while True:
-			if formset.is_valid() is False:
-				has_error = True
-				break
-			## Don't save the instance yet
-			purchase_instances = formset.save(commit=False)
-			if len(purchase_instances) != 1:
-				has_error = True
-				break
-			## PurchaseItemFormset requires purchase_instance
-			## for correct validation and save
-			formset2 = PurchaseItemFormset(request.POST, prefix='purchaseitem',
-						       instance=purchase_instances[0])
-			if formset2.is_valid() is False:
-				has_error = True
-				break
-			## Save all instances
-			purchase_instances[0].save()
-			purchaseitem_instances = formset2.save(commit=False)
-			for purchaseitem_instance in purchaseitem_instances:
-				purchaseitem_instance.save()
-			## Exit from loop
-			break
-		## HTTP response
-		if has_error is False:
-			messages.success(request, 'Success')
-			return HttpResponseRedirect(reverse('coffee_capsules:index'))
-		else:
-			messages.error(request, 'Error')
-			context = {
-				'formset': formset,
-				'formset2': formset2,
-				'capsule_list': all_capsule_list,
-			}
-			return render(request, template_name, context)
-	#### NOT POST method
-	formset = PurchaseFormSet(queryset=Purchase.objects.none(), prefix='purchase')
-	## Initialize formset with all capsules
-	initial = []
-	for capsule in all_capsule_list:
-		initial.append({'capsule': capsule.id, 'price': capsule.price})
-	formset2 = PurchaseItemFormset(initial=initial, prefix='purchaseitem')
-	## context
-	context = {
-		'formset': formset,
-		'formset2': formset2,
-		'capsule_list': all_capsule_list,
-	}
-	return render(request, template_name, context)
+    template_name = 'coffee_capsules/new_purchase.html'
+    all_capsule_list = Capsule.objects.order_by('pk')
+    n_of_capsules = len(all_capsule_list)
+    ## Use formset factory
+    PurchaseFormSet = modelformset_factory(Purchase, form=PurchaseForm,
+                                           extra=1)
+    PurchaseItemFormset = inlineformset_factory(Purchase, PurchaseItem,
+                                                fk_name="purchase",
+                                                extra=n_of_capsules)
+    #### POST method
+    if request.method == 'POST':
+        formset = PurchaseFormSet(request.POST, prefix='purchase')
+        formset2 = PurchaseItemFormset(request.POST, prefix='purchaseitem')
+        ## For a new instance creation, empty form should not be permitted
+        for i in range(0, formset.total_form_count()):
+            formset.forms[i].empty_permitted = False
+        ## Validation and save
+        has_error = False
+        while True:
+            if formset.is_valid() is False:
+                has_error = True
+                break
+            ## Don't save the instance yet
+            purchase_instances = formset.save(commit=False)
+            if len(purchase_instances) != 1:
+                has_error = True
+                break
+            ## PurchaseItemFormset requires purchase_instance
+            ## for correct validation and save
+            formset2 = PurchaseItemFormset(request.POST, prefix='purchaseitem',
+                               instance=purchase_instances[0])
+            if formset2.is_valid() is False:
+                has_error = True
+                break
+            ## Save all instances
+            purchase_instances[0].save()
+            purchaseitem_instances = formset2.save(commit=False)
+            for purchaseitem_instance in purchaseitem_instances:
+                purchaseitem_instance.save()
+            ## Exit from loop
+            break
+        ## HTTP response
+        if has_error is False:
+            messages.success(request, 'Success')
+            return HttpResponseRedirect(reverse('coffee_capsules:index'))
+        else:
+            messages.error(request, 'Error')
+            context = {
+                'formset': formset,
+                'formset2': formset2,
+                'capsule_list': all_capsule_list,
+            }
+            return render(request, template_name, context)
+    #### NOT POST method
+    formset = PurchaseFormSet(queryset=Purchase.objects.none(),
+                              prefix='purchase')
+    ## Initialize formset with all capsules
+    initial = []
+    for capsule in all_capsule_list:
+        initial.append({'capsule': capsule.id, 'price': capsule.price})
+    formset2 = PurchaseItemFormset(initial=initial, prefix='purchaseitem')
+    ## context
+    context = {
+        'formset': formset,
+        'formset2': formset2,
+        'capsule_list': all_capsule_list,
+    }
+    return render(request, template_name, context)
+
 
 @login_required
 @transaction.commit_on_success
 def request(request, myid):
-	template_name = 'coffee_capsules/request.html'
-	purchase = get_object_or_404(Purchase, pk=myid)
-	purchaseitem_list = purchase.purchaseitem_set.order_by('capsule__pk')
-	available_capsule_list = []
-	for purchaseitem in purchaseitem_list:
-		available_capsule_list.append(purchaseitem.capsule)
-	RequestFormset = modelformset_factory(Request, form=MyRequestForm, extra=len(available_capsule_list))
-	#### POST method
-	if request.method == 'POST':
-		formset = RequestFormset(request.POST)
-		if formset.is_valid():
-			request_instances = formset.save(commit=False)
-			for request_instance in request_instances:
-				request_instance.user = request.user
-				request_instance.save()
-			messages.success(request, 'Success')
-			return HttpResponseRedirect(reverse('coffee_capsules:detail', args=(purchase.id,)))
-		else:
-			messages.error(request, 'Error')
-			context = {
-				'formset': formset,
-			}
-			return render(request, template_name, context)
-	#### NOT POST method
-	## Initialize formset with all capsules
-	initial = []
-	for purchaseitem in purchaseitem_list:
-		initial.append({'purchaseitem': purchaseitem, 'user': request.user})
-	formset = RequestFormset(queryset=Request.objects.none(), initial=initial)
-	context = {
-		'formset': formset,
-	}
-	return render(request, template_name, context)
+    template_name = 'coffee_capsules/request.html'
+    purchase = get_object_or_404(Purchase, pk=myid)
+    purchaseitem_list = purchase.purchaseitem_set.order_by('capsule__pk')
+    available_capsule_list = []
+    for purchaseitem in purchaseitem_list:
+        available_capsule_list.append(purchaseitem.capsule)
+    RequestFormset = modelformset_factory(Request, form=MyRequestForm,
+                                          extra=len(available_capsule_list))
+    #### POST method
+    if request.method == 'POST':
+        formset = RequestFormset(request.POST)
+        if formset.is_valid():
+            request_instances = formset.save(commit=False)
+            for request_instance in request_instances:
+                request_instance.user = request.user
+                request_instance.save()
+            messages.success(request, 'Success')
+            return HttpResponseRedirect(reverse('coffee_capsules:detail',
+                                                args=(purchase.id,)))
+        else:
+            messages.error(request, 'Error')
+            context = {
+                'formset': formset,
+            }
+            return render(request, template_name, context)
+    #### NOT POST method
+    ## Initialize formset with all capsules
+    initial = []
+    for purchaseitem in purchaseitem_list:
+        initial.append({'purchaseitem': purchaseitem, 'user': request.user})
+    formset = RequestFormset(queryset=Request.objects.none(), initial=initial)
+    context = {
+        'formset': formset,
+    }
+    return render(request, template_name, context)
+
 
 @login_required
 @transaction.commit_on_success
 def detail(request, myid):
-	agq = True
-	template_name = 'coffee_capsules/detail.html'
-	purchase = get_object_or_404(Purchase, pk=myid)
-	purchaseitem_list = purchase.purchaseitem_set.order_by('capsule__pk')
-	#### raw db connection
-	cursor = connection.cursor()
-	#### get capsule list
-	query_str_capsule = 'SELECT coffee_capsules_capsule.id, name FROM coffee_capsules_capsule'\
-			+ ' INNER JOIN "coffee_capsules_purchaseitem" ON '\
-			+ ' ("coffee_capsules_capsule"."id" = "coffee_capsules_purchaseitem"."capsule_id")'\
-			+ ' WHERE "coffee_capsules_purchaseitem"."purchase_id" = %s'\
-			+ ' ORDER BY coffee_capsules_capsule.id'
-	cursor.execute(query_str_capsule, [myid])
-	capsule_list = cursor.fetchall()
-	#print("==capsule_list==")
-	#print(capsule_list)
-	#print("================")
-	#### make query string for selection
-	if agq == False:
-		query_str_0 = 'SELECT group_concat(ca, ", ") AS gc'\
-			+ ' FROM (SELECT'\
-			+ ' "SUM(CASE WHEN capsule_id=" || coffee_capsules_capsule.id || " THEN coffee_capsules_request.quantity_accepted ELSE 0 END) AS qty_a_" || coffee_capsules_capsule.id AS ca'\
-			+ ' FROM coffee_capsules_capsule'\
-			+ ' INNER JOIN "coffee_capsules_purchaseitem" ON '\
-			+ ' ("coffee_capsules_capsule"."id" = "coffee_capsules_purchaseitem"."capsule_id")'\
-			+ ' WHERE "coffee_capsules_purchaseitem"."purchase_id" = %s'\
-			+ ' ORDER BY coffee_capsules_capsule.id)'
-		cursor.execute(query_str_0, [myid])
-		select_str = cursor.fetchall()[0][0]
-	else:
-		query_str_0 = 'SELECT group_concat(ca || ", " || cg || ", " || cq, ", ") AS gc'\
-			+ ' FROM (SELECT'\
-			+ ' "SUM(CASE WHEN capsule_id=" || coffee_capsules_capsule.id || " THEN coffee_capsules_request.quantity_accepted ELSE 0 END) AS qty_a_" || coffee_capsules_capsule.id AS ca'\
-			+ ', "SUM(CASE WHEN capsule_id=" || coffee_capsules_capsule.id || " THEN coffee_capsules_request.quantity_grouped ELSE 0 END) AS qty_g_" || coffee_capsules_capsule.id AS cg'\
-			+ ', "SUM(CASE WHEN capsule_id=" || coffee_capsules_capsule.id || " THEN coffee_capsules_request.quantity_queued ELSE 0 END) AS qty_q_" || coffee_capsules_capsule.id AS cq'\
-			+ ' FROM coffee_capsules_capsule'\
-			+ ' INNER JOIN "coffee_capsules_purchaseitem" ON '\
-			+ ' ("coffee_capsules_capsule"."id" = "coffee_capsules_purchaseitem"."capsule_id")'\
-			+ ' WHERE "coffee_capsules_purchaseitem"."purchase_id" = %s'\
-			+ ' ORDER BY coffee_capsules_capsule.id)'
-		cursor.execute(query_str_0, [myid])
-		select_str = cursor.fetchall()[0][0]
-	if select_str is None:
-		select_str = 'COUNT(*)'
-	#### get pivot table
-	query_str_1 = 'SELECT auth_user.username, ' + select_str
-	if agq == False:
-		query_str_1 += ', SUM(coffee_capsules_request.quantity_accepted) AS SUM_a'\
-			+ ', SUM(coffee_capsules_purchaseitem.price * coffee_capsules_request.quantity_accepted) AS GT_a'
-	else:
-		query_str_1 += ', SUM(coffee_capsules_request.quantity_accepted) AS SUM_a'\
-			+ ', SUM(coffee_capsules_request.quantity_grouped) AS SUM_a'\
-			+ ', SUM(coffee_capsules_request.quantity_queued) AS SUM_a'\
-			+ ', SUM(coffee_capsules_purchaseitem.price * coffee_capsules_request.quantity_accepted) AS GT_a'\
-			+ ', SUM(coffee_capsules_purchaseitem.price * coffee_capsules_request.quantity_grouped) AS GT_a'\
-			+ ', SUM(coffee_capsules_purchaseitem.price * coffee_capsules_request.quantity_queued) AS GT_a'
-	query_str_1 += ' FROM "coffee_capsules_request"'\
-		+ ' INNER JOIN "coffee_capsules_purchaseitem" ON '\
-		+ ' ("coffee_capsules_request"."purchaseitem_id" = "coffee_capsules_purchaseitem"."id")'\
-		+ ' INNER JOIN "auth_user" ON ("coffee_capsules_request"."user_id" = "auth_user"."id")'\
-		+ ' WHERE "coffee_capsules_purchaseitem"."purchase_id" = %s'\
-		+ ' GROUP BY coffee_capsules_request.user_id'
-	cursor.execute(query_str_1, [myid])
-	request_list = cursor.fetchall()
-	#print("==request_list==")
-	#for i in request_list:
-	#	print(i)
-	#	print("----------------")
-	#print("================")
-	#### get total row
-	zipped = zip(*request_list)
-	total_row = [0 for i in range(len(zipped))]
-	#print(zipped)
-	for i in range(1,len(zipped)):
-		total_row[i] = zipped[i][0]
-		for j in range(1,len(zipped[i])):
-			total_row[i] += zipped[i][1]
-	if len(zipped) > 0:
-		total_row[0] = "Total"
-	##### context
-	context = {
-		'agq': agq,
-		'purchase': purchase,
-		'purchaseitem_list': purchaseitem_list,
-		'capsule_list': capsule_list,
-		'request_list': request_list,
-		'total_row': total_row,
-		}
-	return render(request, template_name, context)
+    agq = True
+    template_name = 'coffee_capsules/detail.html'
+    purchase = get_object_or_404(Purchase, pk=myid)
+    purchaseitem_list = purchase.purchaseitem_set.order_by('capsule__pk')
+    #### raw db connection
+    cursor = connection.cursor()
+    #### get capsule list
+    query_str_capsule = 'SELECT coffee_capsules_capsule.id, name FROM coffee_capsules_capsule'\
+            + ' INNER JOIN "coffee_capsules_purchaseitem" ON '\
+            + ' ("coffee_capsules_capsule"."id" = "coffee_capsules_purchaseitem"."capsule_id")'\
+            + ' WHERE "coffee_capsules_purchaseitem"."purchase_id" = %s'\
+            + ' ORDER BY coffee_capsules_capsule.id'
+    cursor.execute(query_str_capsule, [myid])
+    capsule_list = cursor.fetchall()
+    #print("==capsule_list==")
+    #print(capsule_list)
+    #print("================")
+    #### make query string for selection
+    if not agq:
+        query_str_0 = 'SELECT group_concat(ca, ", ") AS gc'\
+            + ' FROM (SELECT'\
+            + ' "SUM(CASE WHEN capsule_id=" || coffee_capsules_capsule.id || " THEN coffee_capsules_request.quantity_accepted ELSE 0 END) AS qty_a_" || coffee_capsules_capsule.id AS ca'\
+            + ' FROM coffee_capsules_capsule'\
+            + ' INNER JOIN "coffee_capsules_purchaseitem" ON '\
+            + ' ("coffee_capsules_capsule"."id" = "coffee_capsules_purchaseitem"."capsule_id")'\
+            + ' WHERE "coffee_capsules_purchaseitem"."purchase_id" = %s'\
+            + ' ORDER BY coffee_capsules_capsule.id)'
+        cursor.execute(query_str_0, [myid])
+        select_str = cursor.fetchall()[0][0]
+    else:
+        query_str_0 = 'SELECT group_concat(ca || ", " || cg || ", " || cq, ", ") AS gc'\
+            + ' FROM (SELECT'\
+            + ' "SUM(CASE WHEN capsule_id=" || coffee_capsules_capsule.id || " THEN coffee_capsules_request.quantity_accepted ELSE 0 END) AS qty_a_" || coffee_capsules_capsule.id AS ca'\
+            + ', "SUM(CASE WHEN capsule_id=" || coffee_capsules_capsule.id || " THEN coffee_capsules_request.quantity_grouped ELSE 0 END) AS qty_g_" || coffee_capsules_capsule.id AS cg'\
+            + ', "SUM(CASE WHEN capsule_id=" || coffee_capsules_capsule.id || " THEN coffee_capsules_request.quantity_queued ELSE 0 END) AS qty_q_" || coffee_capsules_capsule.id AS cq'\
+            + ' FROM coffee_capsules_capsule'\
+            + ' INNER JOIN "coffee_capsules_purchaseitem" ON '\
+            + ' ("coffee_capsules_capsule"."id" = "coffee_capsules_purchaseitem"."capsule_id")'\
+            + ' WHERE "coffee_capsules_purchaseitem"."purchase_id" = %s'\
+            + ' ORDER BY coffee_capsules_capsule.id)'
+        cursor.execute(query_str_0, [myid])
+        select_str = cursor.fetchall()[0][0]
+    if select_str is None:
+        select_str = 'COUNT(*)'
+    #### get pivot table
+    query_str_1 = 'SELECT auth_user.username, ' + select_str
+    if not agq:
+        query_str_1 += ', SUM(coffee_capsules_request.quantity_accepted) AS SUM_a'\
+            + ', SUM(coffee_capsules_purchaseitem.price * coffee_capsules_request.quantity_accepted) AS GT_a'
+    else:
+        query_str_1 += ', SUM(coffee_capsules_request.quantity_accepted) AS SUM_a'\
+            + ', SUM(coffee_capsules_request.quantity_grouped) AS SUM_a'\
+            + ', SUM(coffee_capsules_request.quantity_queued) AS SUM_a'\
+            + ', SUM(coffee_capsules_purchaseitem.price * coffee_capsules_request.quantity_accepted) AS GT_a'\
+            + ', SUM(coffee_capsules_purchaseitem.price * coffee_capsules_request.quantity_grouped) AS GT_a'\
+            + ', SUM(coffee_capsules_purchaseitem.price * coffee_capsules_request.quantity_queued) AS GT_a'
+    query_str_1 += ' FROM "coffee_capsules_request"'\
+        + ' INNER JOIN "coffee_capsules_purchaseitem" ON '\
+        + ' ("coffee_capsules_request"."purchaseitem_id" = "coffee_capsules_purchaseitem"."id")'\
+        + ' INNER JOIN "auth_user" ON ("coffee_capsules_request"."user_id" = "auth_user"."id")'\
+        + ' WHERE "coffee_capsules_purchaseitem"."purchase_id" = %s'\
+        + ' GROUP BY coffee_capsules_request.user_id'
+    cursor.execute(query_str_1, [myid])
+    request_list = cursor.fetchall()
+    #print("==request_list==")
+    #for i in request_list:
+    #    print(i)
+    #    print("----------------")
+    #print("================")
+    #### get total row
+    zipped = zip(*request_list)
+    total_row = [0 for i in range(len(zipped))]
+    #print(zipped)
+    for i in range(1, len(zipped)):
+        total_row[i] = zipped[i][0]
+        for j in range(1, len(zipped[i])):
+            total_row[i] += zipped[i][1]
+    if len(zipped) > 0:
+        total_row[0] = "Total"
+    ##### context
+    context = {
+        'agq': agq,
+        'purchase': purchase,
+        'purchaseitem_list': purchaseitem_list,
+        'capsule_list': capsule_list,
+        'request_list': request_list,
+        'total_row': total_row,
+        }
+    return render(request, template_name, context)
